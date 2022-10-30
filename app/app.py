bc02d58d5eb68d183b8b3e90d8b51fa8bb9c20a3
Alvid Nguyen <d.fed22@gsoft01.tk>
2022-10-30 23:22:25 +0700

    update page

--- app/app.py
+++ app/app.py
@@ -1,25 +1,36 @@
 from flask import render_template, request
 from app import app
-from controller import getCategory
-import sys
+from controller import getCategory, getProduct, getUser, addCategory
 
-
+# index
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/<type>')
-def route(type=None):
-    data = []
-    if type == 'user':
-        pass
-    elif type == 'product':
-        pass
-    elif type == 'category':
-        data = getCategory()
+# user
+@app.route('/user')
+def user(type=None):
+    data = getUser()
         
-    return render_template(type + '.html', data=data)
+    return render_template('user.html', data=data)
 
+# product
+@app.route('/product')
+def product(type=None):
+    data = getProduct()
+        
+    return render_template('product.html', data=data)
+
+# category
+@app.route('/category', methods=['POST','GET'])
+def category():
+
+    if request.method == 'POST':
+        name = request.form['category']
+        addCategory(name=name)
+        
+    data = getCategory()
+    return render_template('category.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
